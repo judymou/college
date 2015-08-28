98b9617f98216bf8a1dbfbd4c8d0130065adabe0
Judy Mou <moujudy@gmail.com>
2015-08-28 00:53:15 -0700

    listen to system port

--- college_app.js
+++ college_app.js
@@ -21,7 +21,7 @@ function serveFile(path, res) {
   });
 }
 
-var server = app.listen(5000, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
