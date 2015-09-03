f468fc555a19b0a57eca16209a4aa98031ba1f47
Judy Mou <moujudy@gmail.com>
2015-09-02 22:26:06 -0700

    hookup signup

--- college_app.js
+++ college_app.js
@@ -1,12 +1,34 @@
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
+var gsheet = require("google-spreadsheet");
+var gsheetCred = require('./google-generated-creds.json');
 var app = express();
+
+var sheet = new gsheet('1EwcpAOYA4PuE6a8H_oNUl-5VdTv4yPa2BPJQ1_ekO1M');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   serveFile('index.html', res);
+});
+
+app.post('/signup', function(req, res) {
+  console.log(req);
+  sheet.useServiceAccountAuth(gsheetCred, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      sheet.addRow(1, {
+         name: req.name,
+         email: req.email,
+         question: req.question || ''
+      });
+      res.send({success: true});
+      return;
+    }
+    res.send({success: false});
+  })
 });
 
 function serveFile(path, res) {
--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,16 @@
 function App() {
   var me = this;
   App.prototype.init = function() {
-    /*
-    $('.bxslider').bxSlider({
-      auto: true,
-      captions: true,
-      mode: 'fade'
+    $('#signup').on('click', function() {
+      $.post('/signup', {name: 'judyclient', email: 'judy email', question: 'blah'},
+        function(data) {
+          if (data.success) {
+            alert('Thank you for signing up.');
+          } else {
+            alert('Sorry, cannot sign you up. Please come back later.');
+          }
+        });
     });
-    */
   }
 }
 $(function() {
