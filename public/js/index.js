d3d0f8cb0ee6fe6fc77c9bf6d79099da834f901f
Judy Mou <moujudy@gmail.com>
2015-10-25 21:34:36 -0700

    fix email signup bug

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 function App() {
+  var me = this;
   App.prototype.init = function() {
     $('#signup').on('click', function() {
      var docHeight = $(document).height();
@@ -8,13 +9,25 @@ function App() {
      $('.popup').show().css({'position': 'fixed'});
     });
 
+    $('#start-talking').on('click', function() {
+      $('html, body').animate({
+          scrollTop: $('#three').offset().top
+      }, 1000);
+    });
+
     $('.close-popup, .overlay-bg').click(function() {
       $('.overlay-bg, .overlay-content').hide();
     });
 
     $('#send-message').on('click', function() {
       event.preventDefault();
-      $.post('/signup', {name: $('#namepopup').val(), email: $('#emailpopup').val(), question: $('#messagepopup').val()},
+      if (!$('#namepopup').val() || !$('#emailpopup').val() || !$('#messagepopup').val()) {
+        alert('Sorry, cannot sign you up. Some required information is missing.');
+        return;
+      }
+      $.post('/signup', 
+             {name: $('#namepopup').val(), email: $('#emailpopup').val(),
+              question: $('#messagepopup').val()},
         function(data) {
           if (data.success) {
             alert('Thank you for signing up.');
@@ -26,6 +39,10 @@ function App() {
     });
     $('#send-message-footer').on('click', function() {
       event.preventDefault();
+      if (!$('#name').val() || !$('#email').val() || !$('#message').val()) {
+        alert('Sorry, cannot sign you up. Some required information is missing.');
+        return;
+      }
       $.post('/signup', {name: $('#name').val(), email: $('#email').val(), question: $('#message').val()},
         function(data) {
           if (data.success) {
@@ -36,7 +53,10 @@ function App() {
           $('.overlay-bg, .overlay-content').hide();
         });
     });
-  }
+  };
+
+  App.prototype._signUp = function() {
+  };
 }
 $(function() {
   window.app = new App();
